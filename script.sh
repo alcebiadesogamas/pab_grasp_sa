62301ae71ea98ddd2beb936c738c0915cfc8dd27
Alcebiades <alcebiades.29@gmail.com>
2023-06-01 13:26:30 -0300

    fix: ajuste no script d execucao.

--- script.sh
+++ script.sh
@@ -18,29 +18,49 @@ executar() {
         do 
             case "$i" in
                 1) 
-                    echo "instance: i0"$j".txt"               
-                    echo "alpha equals 0"
-                    rodar 0 "i0"$j".txt"
+                    for((k=1;k<=5;k++))
+                    do
+                        echo "instance: i0"$j".txt"               
+                        echo "alpha equals 0"
+                        rodar 0 "i0"$j".txt"
+                        sleep 1
+                    done
                     ;;
                 2)
-                    echo "instance: i0"$j".txt"               
-                    echo "alpha equals 0.2"
-                    rodar 0.2 "i0"$j".txt"
+                    for((k=1;k<=5;k++))
+                    do
+                        echo "instance: i0"$j".txt"               
+                        echo "alpha equals 0.2"
+                        rodar 0.2 "i0"$j".txt"
+                        sleep 1
+                    done
                     ;;
                 3)
-                    echo "instance: i0"$j".txt"               
-                    echo "alpha equals 0.5"
-                    rodar 0.5 "i0"$j".txt"
+                    for((k=1;k<=5;k++))
+                    do
+                        echo "instance: i0"$j".txt"               
+                        echo "alpha equals 0.5"
+                        rodar 0.5 "i0"$j".txt"
+                        sleep 1
+                    done
                     ;;
                 4)
-                    echo "instance: i0"$j".txt"               
-                    echo "alpha equals 0.8"
-                    rodar 0.8 "i0"$j".txt"
+                    for((k=1;k<=5;k++))
+                    do
+                        echo "instance: i0"$j".txt"               
+                        echo "alpha equals 0.8"
+                        rodar 0.8 "i0"$j".txt"
+                        sleep 1
+                    done
                     ;;
                 5)
-                    echo "instance: i0"$j".txt"               
-                    echo "alpha equals 1"
-                    rodar 1 "i0"$j".txt"
+                    for((k=1;k<=5;k++))
+                    do                
+                        echo "instance: i0"$j".txt"               
+                        echo "alpha equals 1"
+                        rodar 1 "i0"$j".txt"
+                        sleep 1
+                    done
                     ;;
                 *) echo default
                 ;;
